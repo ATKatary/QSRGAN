85a57b84c935bfe6894a397486020f5ca27c0d63
Ahmed <akatary@mit.edu>
2022-06-30 08:36:35 -0400

    Adding pretrained srgan

--- networks/srcnngan/implementation/srcnn.py
+++ networks/srcnngan/implementation/srcnn.py
@@ -31,12 +31,20 @@ class SRCNN(nn.Module):
         
         return self.net(f(input))
     
-    def initiate(self, device, pretrained_weights):
-        """ Initializes the model using pretrained weights """
+    def initiate_data_parallel(self, device, pretrained_weights):
+        """ Initializes the model using data parallel pretrained weights """
         print("Initializing ...")
         model = nn.DataParallel(self).to(device)
         model.load_state_dict(pretrained_weights)
         model.eval()
         return model
+    
+    def initiate(self, device, pretrained_weights):
+        """ Initializes the model using pretrained weights """
+        print("Initializing ...")
+        self.load_state_dict(pretrained_weights)
+        self.eval()
+        return self
+
 
         
